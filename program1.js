8fd978076a94d8f7466131ea0c4e5197af83ab61
Vinay7987 <96689517+Vinay7987@users.noreply.github.com>
2024-01-05 13:15:30 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const bracketMapping = {')': '(', '}': '{', ']': '['};
+
+    for (const char of s) {
+        if (bracketMapping[char] === undefined) {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== bracketMapping[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
