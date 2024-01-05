e7f2513ac081cd57f7885bea1dc98e6b6893165c
Vinay7987 <96689517+Vinay7987@users.noreply.github.com>
2024-01-05 13:17:17 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,40 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    /**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanValues[s[i]];
+        const nextVal = romanValues[s[i + 1]];
+
+        if (nextVal > currentVal) {
+            result += nextVal - currentVal;
+            i++; // Skip the next character since it has already been considered
+        } else {
+            result += currentVal;
+        }
+    }
+
+    return result;
+};
+
+module.exports = { romanToInt };
+
 };
 
 
